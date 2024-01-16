b293388a75ff9b2773b4d5937580cb41b8b07f1f
Dmitry Lyfar <dmitry.lyfar@canonical.com>
2024-01-17 12:45:04 +1300

    Fix linter issues

--- sdkcraft/application.py
+++ sdkcraft/application.py
@@ -15,7 +15,6 @@
 #  with this program.  If not, see <http://www.gnu.org/licenses/>.
 """Main application class for sdkcraft."""
 import copy
-import pathlib
 from typing import Any
 
 import craft_parts
